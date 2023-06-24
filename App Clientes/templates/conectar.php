3c2976612cea7cbe6f2c7c60e07e7a56561efdbe
judithmarg <87728150+judithmarg@users.noreply.github.com>
2023-06-24 18:51:16 -0400

    Create conectar.php

---
+++ App Clientes/templates/conectar.php
@@ -0,0 +1,10 @@
+<?php
+ $servername="LAPTOP-DP5KPM1T";
+ $conexion=array("Database"=>"TALLER-FINAL", 
+                "UID"=>"admin",
+                "PWD"=>"admin123",
+                "CharacterSet"=>"UTF-8");
+
+$con=sqlsrv_connect($servername,$conexion);
+
+?>
